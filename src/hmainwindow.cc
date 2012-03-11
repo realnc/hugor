754f148696023c0d1a965f33a64fa0edd3c244e9
Nikos Chantziaras <realnc@gmail.com>
2012-03-12 00:19:24 +0200

    Revert "Don't check whether a game is running when handling a QCloseEvent"
    This reverts commit 068778648b13013db4e7fdb72ba78015beff86d4.
    
    It turns out we still need that check so that HApplication::main() can close
    the main window when the game has ended.

--- src/hmainwindow.cc
+++ src/hmainwindow.cc
@@ -152,6 +152,10 @@ HMainWindow::fShowScrollback()
 void
 HMainWindow::closeEvent( QCloseEvent* e )
 {
+    if (not hApp->gameRunning()) {
+        return;
+    }
+
     QMessageBox* msgBox = new QMessageBox(QMessageBox::Question,
                                           tr("Quit Hugor"),
                                           tr("Abandon the story and quit the application?"),
