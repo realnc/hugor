a55b3c3ac61ad34f38c836d23f20ce1828bae622
Nikos Chantziaras <realnc@gmail.com>
2012-03-18 10:54:59 +0200

    Bump version to 0.9

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define HUGOR_VERSION "0.8.1"
-#define W32_RC_FILEVERSION 0,8,1,0
-#define W32_RC_PRODUCTVERSION 0,8,1,0
+#define HUGOR_VERSION "0.9"
+#define W32_RC_FILEVERSION 0,9,0,0
+#define W32_RC_PRODUCTVERSION 0,9,0,0
 
 #endif
