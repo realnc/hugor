efc15c2085b5c10cb10c6ff3a3d6ef658527d3c0
Nikos Chantziaras <realnc@gmail.com>
2013-06-12 08:56:40 +0300

    Mark version as "git"

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define HUGOR_VERSION "1.0"
-#define W32_RC_FILEVERSION 1,0,0,0
-#define W32_RC_PRODUCTVERSION 1,0,0,0
+#define HUGOR_VERSION "1.0 git"
+#define W32_RC_FILEVERSION 1,0,0,99
+#define W32_RC_PRODUCTVERSION 1,0,0,99
 
 #endif
